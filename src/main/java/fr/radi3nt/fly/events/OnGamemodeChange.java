3247b57fd40b25a8f5aa0ecb237830ee12d6f2c3
Radi3nt <redwhite.faction200@gmail.com>
2020-05-18 11:31:11 +0200

    Gamemode change bug corrected

--- src/main/java/fr/radi3nt/fly/events/OnGamemodeChange.java
+++ src/main/java/fr/radi3nt/fly/events/OnGamemodeChange.java
@@ -1,5 +1,6 @@
 package fr.radi3nt.fly.events;
 
+import fr.radi3nt.fly.MainFly;
 import fr.radi3nt.fly.commands.Fly;
 import fr.radi3nt.fly.commands.Tempfly;
 import org.bukkit.GameMode;
@@ -7,11 +8,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
 import java.util.Map;
 
+import static fr.radi3nt.fly.timer.checker.timem;
+
 public class OnGamemodeChange implements Listener {
+
+    Plugin plugin = MainFly.getPlugin(MainFly.class);
 
     ArrayList<String> flyers = Fly.flyers;
     public Map<String, Long> timer = Tempfly.timer;
@@ -19,25 +25,21 @@ public class OnGamemodeChange implements Listener {
 
     @EventHandler
     public void OnGamemodeChange(PlayerGameModeChangeEvent e) {
+        GameMode gamemode = e.getNewGameMode();
         Player player = e.getPlayer();
         player.setInvulnerable(false);
-        if (!player.hasPermission("fly.gamemode")) {
-            if (!(player.getGameMode( ) == GameMode.CREATIVE)) {
+        if (gamemode.equals(GameMode.CREATIVE)) {
+                player.setAllowFlight(true);
+                player.setFlying(true);
+        } else if (player.getGameMode().equals(GameMode.CREATIVE)) {
+            if (!player.hasPermission("fly.gamemode")) {
                 player.setAllowFlight(false);
                 player.setFlying(false);
+                time.remove(player);
+                timer.remove(player);
+                timem.remove(player);
                 flyers.remove(player.getName());
-                timer.remove(player.getName());
-                time.remove(player.getName());
-            } else if (player.getGameMode() == GameMode.CREATIVE) {
-                player.setFlying(true);
             }
-        } else {
-            player.setAllowFlight(false);
-            player.setFlying(false);
-            flyers.remove(player.getName());
-            timer.remove(player.getName());
-            time.remove(player.getName());
         }
     }
-
 }
