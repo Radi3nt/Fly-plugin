e7f5635bd91e490c89160d7602a46e5786ffd443
Radi3nt <redwhite.faction200@gmail.com>
2020-06-07 21:02:23 +0200

    Optimizations

--- src/main/java/fr/radi3nt/fly/commands/FlyGui.java
+++ src/main/java/fr/radi3nt/fly/commands/FlyGui.java
@@ -53,42 +53,42 @@ public class FlyGui implements CommandExecutor {
                 List<Player> list = new ArrayList<>(Bukkit.getOnlinePlayers());
                 if (player.hasPermission("fly.gui")) {
                     if (list.size() < 54) {
-                            Inventory playerlist = Bukkit.createInventory(player, 54, ChatColor.GOLD + "         === Player list ===");
-                            for (int i = 0; i < list.size(); i++) {
+                        Inventory playerlist = Bukkit.createInventory(player, 54, ChatColor.GOLD + "         === Player list ===");
+                        for (int i = 0; i < list.size(); i++) {
 
-                                Player target = list.get(i);
-                                ItemStack playerhead = new ItemStack(Material.PLAYER_HEAD, 1, (short) 3);
-                                SkullMeta sk = (SkullMeta) playerhead.getItemMeta();
-                                ArrayList<String> loreh = new ArrayList<>();
-                                if (target.getAllowFlight()) {
-                                    sk.setDisplayName(ChatColor.GREEN + target.getName());
-                                    if (target.isFlying()) {
-                                        loreh.add(ChatColor.GREEN + target.getName() + " is flying");
-                                    } else {
-                                        loreh.add(ChatColor.DARK_RED + target.getName() + " isn't flying");
-                                    }
-                                    sk.setLore(loreh);
+                            Player target = list.get(i);
+                            ItemStack playerhead = new ItemStack(Material.PLAYER_HEAD, 1, (short) 3);
+                            SkullMeta sk = (SkullMeta) playerhead.getItemMeta();
+                            ArrayList<String> loreh = new ArrayList<>();
+                            if (target.getAllowFlight()) {
+                                sk.setDisplayName(ChatColor.GREEN + target.getName());
+                                if (target.isFlying()) {
+                                    loreh.add(ChatColor.GREEN + target.getName() + " is flying");
                                 } else {
-                                    sk.setDisplayName(ChatColor.DARK_RED + target.getName());
                                     loreh.add(ChatColor.DARK_RED + target.getName() + " isn't flying");
-                                    sk.setLore(loreh);
                                 }
-                                sk.setOwner(target.getName());
-                                playerhead.setItemMeta(sk);
-
-                                playerlist.addItem(playerhead);
-
-
+                                sk.setLore(loreh);
+                            } else {
+                                sk.setDisplayName(ChatColor.DARK_RED + target.getName());
+                                loreh.add(ChatColor.DARK_RED + target.getName() + " isn't flying");
+                                sk.setLore(loreh);
                             }
-                            player.openInventory(playerlist);
-                        } else {
-                            sender.sendMessage(Prefix + ChatColor.RED + " " + InvalidPlayer);
+                            sk.setOwner(target.getName());
+                            playerhead.setItemMeta(sk);
+
+                            playerlist.addItem(playerhead);
+
+
                         }
+                        player.openInventory(playerlist);
                     } else {
-                        sender.sendMessage(Prefix + ChatColor.RED + " " + NoPermission);
+                        sender.sendMessage(Prefix + ChatColor.RED + " " + InvalidPlayer);
                     }
+                } else {
+                    sender.sendMessage(Prefix + ChatColor.RED + " " + NoPermission);
                 }
-            } else {
+            }
+        } else {
             sender.sendMessage(Prefix + ChatColor.RED + " This command MUST be run by a player");
         }
         return true;
