6f534c7dba51ad442291f3c61bcdbba2d23dc517
Radi3nt <redwhite.faction200@gmail.com>
2020-06-12 19:59:06 +0200

    Changed potion obtaining system

--- src/main/java/fr/radi3nt/fly/events/crafts/OnCraftEvent.java
+++ src/main/java/fr/radi3nt/fly/events/crafts/OnCraftEvent.java
@@ -2,19 +2,15 @@ package fr.radi3nt.fly.events.crafts;
 
 import fr.radi3nt.fly.MainFly;
 import org.bukkit.ChatColor;
-import org.bukkit.Color;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.CraftingInventory;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.PotionMeta;
 
+import static fr.radi3nt.fly.events.crafts.PrepareCraftItemEvent.CreateFlyPotion;
 import static fr.radi3nt.fly.events.crafts.PrepareCraftItemEvent.PotionCraft;
 
 public class OnCraftEvent implements Listener {
@@ -29,6 +25,7 @@ public class OnCraftEvent implements Listener {
                     if (e.getCurrentItem().getType() != Material.AIR) {
                         if (e.getCurrentItem().getItemMeta().getDisplayName().contains(ChatColor.GOLD + "Fly Potion ")) {
                             ItemStack[] matrix = inventory.getMatrix();
+                            Material Base = matrix[0].getType();
                             for (ItemStack itemStack : matrix) {
                                 if (itemStack.getType().equals(matrix[4].getType())) {
                                     itemStack.setAmount(itemStack.getAmount() - 1);
@@ -40,31 +37,37 @@ public class OnCraftEvent implements Listener {
                                     }
                                 }
                             }
+                            int temps = 0;
+                            int i = (PotionCraft.get(inventory) - 1);
+                            int emerald = 15;
+                            int diamond = 5;
+                            int gold = 1;
+                            switch (Base) {
+                                case EMERALD:
+                                    temps = i * emerald + emerald;
+                                    break;
+                                case EMERALD_BLOCK:
+                                    temps = i * emerald * 9 + i * emerald * 9;
+                                    break;
+                                case DIAMOND:
+                                    temps = i * diamond + diamond;
+                                    break;
+                                case DIAMOND_BLOCK:
+                                    temps = i * diamond * 9 + diamond * 9;
+                                    break;
+                                case GOLD_INGOT:
+                                    temps = i * gold + gold;
+                                    break;
+                                case GOLD_BLOCK:
+                                    temps = i * gold * 9 + gold * 9;
+                                    break;
+                            }
                             inventory.setMatrix(matrix);
-                            int temps = (PotionCraft.get(inventory) - 1) * 15 + 15;
                             int heures = (temps / 3600);
                             int minutes = ((temps - (temps / 3600) * 3600) / 60);
                             int seconds = temps - (heures * 3600 + minutes * 60);
 
-                            ItemStack item = new ItemStack(Material.POTION, 1);
-                            ItemMeta meta = item.getItemMeta();
-                            if (heures < 1) {
-                                if (minutes < 1) {
-                                    meta.setDisplayName(ChatColor.GOLD + "Fly Potion " + seconds + " sec");
-                                } else {
-                                    meta.setDisplayName(ChatColor.GOLD + "Fly Potion " + minutes + " min " + seconds + " sec");
-                                }
-                            } else {
-                                meta.setDisplayName(ChatColor.GOLD + "Fly Potion " + heures + " h " + minutes + " min " + seconds + " sec");
-                            }
-                            meta.addEnchant(Enchantment.DURABILITY, 1, false);
-                            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-                            meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
-                            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-                            item.setItemMeta(meta);
-                            PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
-                            potionMeta.setColor(Color.FUCHSIA);
-                            item.setItemMeta(potionMeta);
+                            ItemStack item = CreateFlyPotion(heures, minutes, seconds);
                             e.getWhoClicked().setItemOnCursor(item);
                             PotionCraft.remove(inventory);
 
@@ -72,8 +75,6 @@ public class OnCraftEvent implements Listener {
                     }
                 }
             }
-        } else {
-            e.setCancelled(true);
         }
     }
 
--- src/main/java/fr/radi3nt/fly/events/crafts/PrepareCraftItemEvent.java
+++ src/main/java/fr/radi3nt/fly/events/crafts/PrepareCraftItemEvent.java
@@ -20,8 +20,28 @@ public class PrepareCraftItemEvent implements Listener {
 
     public static HashMap<CraftingInventory, Integer> PotionCraft = new HashMap<>();
 
-    public static boolean CheckForEmerauldMateral(ItemStack[] craftingitems, int number) {
-        Material E = Material.EMERALD;
+    public static boolean CheckForEmerauldMateral(ItemStack[] craftingitems, int number, int g) {
+        Material E = Material.BEDROCK;
+        switch (g) {
+            case 0:
+                E = Material.EMERALD;
+                break;
+            case 1:
+                E = Material.EMERALD_BLOCK;
+                break;
+            case 2:
+                E = Material.DIAMOND;
+                break;
+            case 3:
+                E = Material.DIAMOND_BLOCK;
+                break;
+            case 4:
+                E = Material.GOLD_INGOT;
+                break;
+            case 5:
+                E = Material.GOLD_BLOCK;
+                break;
+        }
         Material B = Material.GLASS_BOTTLE;
 
         for (int l = 0; l < craftingitems.length; l++) {
@@ -41,51 +61,82 @@ public class PrepareCraftItemEvent implements Listener {
         return true;
     }
 
+    public static ItemStack CreateFlyPotion(int heures, int minutes, int seconds) {
+        ItemStack item = new ItemStack(Material.POTION, 1);
+        ItemMeta meta = item.getItemMeta();
+        if (heures < 1) {
+            if (minutes < 1) {
+                meta.setDisplayName(ChatColor.GOLD + "Fly Potion " + seconds + " sec");
+            } else {
+                meta.setDisplayName(ChatColor.GOLD + "Fly Potion " + minutes + " min " + seconds + " sec");
+            }
+        } else {
+            meta.setDisplayName(ChatColor.GOLD + "Fly Potion " + heures + " h " + minutes + " min " + seconds + " sec");
+        }
+        meta.addEnchant(Enchantment.DURABILITY, 1, false);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        item.setItemMeta(meta);
+        PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+        potionMeta.setColor(Color.FUCHSIA);
+        item.setItemMeta(potionMeta);
+        return item;
+    }
+
     @EventHandler
     public void PrepareCraftItemEvent(PrepareItemCraftEvent e) {
         if (MainFly.getPlugin(MainFly.class).getConfig().getBoolean("fly-potions")) {
-            ItemStack[] craftingitems = e.getInventory().getMatrix();
-            ItemStack finalitem = new ItemStack(Material.AIR);
-            int z = 0;
-            for (int i = 0; i <= 64; i++) {
-                int temps = i * 15 + 15;
-                int heures = (temps / 3600);
-                int minutes = ((temps - (temps / 3600) * 3600) / 60);
-                int seconds = temps - (heures * 3600 + minutes * 60);
-
-                ItemStack item = new ItemStack(Material.POTION, 1);
-                ItemMeta meta = item.getItemMeta();
-                if (heures < 1) {
-                    if (minutes < 1) {
-                        meta.setDisplayName(ChatColor.GOLD + "Fly Potion " + seconds + " sec");
-                    } else {
-                        meta.setDisplayName(ChatColor.GOLD + "Fly Potion " + minutes + " min " + seconds + " sec");
+            for (int l = 0; l < 6; l++) {
+                ItemStack[] craftingitems = e.getInventory().getMatrix();
+                ItemStack finalitem = new ItemStack(Material.AIR);
+                int z = 0;
+                int emerald = 15;
+                int diamond = 5;
+                int gold = 1;
+                for (int i = 0; i <= 64; i++) {
+                    int temps = i;
+                    switch (l) {
+                        case 0:
+                            temps = i * emerald + emerald;
+                            break;
+                        case 1:
+                            temps = i * emerald * 9 + i * emerald * 9;
+                            break;
+                        case 2:
+                            temps = i * diamond + diamond;
+                            break;
+                        case 3:
+                            temps = i * diamond * 9 + diamond * 9;
+                            break;
+                        case 4:
+                            temps = i * gold + gold;
+                            break;
+                        case 5:
+                            temps = i * gold * 9 + gold * 9;
+                            break;
                     }
-                } else {
-                    meta.setDisplayName(ChatColor.GOLD + "Fly Potion " + heures + " h " + minutes + " min " + seconds + " sec");
-                }
-                meta.addEnchant(Enchantment.DURABILITY, 1, false);
-                meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-                meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
-                meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-                item.setItemMeta(meta);
-                PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
-                potionMeta.setColor(Color.FUCHSIA);
-                item.setItemMeta(potionMeta);
+                    int heures = (temps / 3600);
+                    int minutes = ((temps - (temps / 3600) * 3600) / 60);
+                    int seconds = temps - (heures * 3600 + minutes * 60);
 
-                if (CheckForEmerauldMateral(craftingitems, i + 1)) {
-                    finalitem.setType(item.getType());
-                    finalitem.setItemMeta(item.getItemMeta());
-                    e.getInventory().setResult(null);
-                    PotionCraft.remove(e.getInventory());
-                    z = i;
-                }
+                    ItemStack item = CreateFlyPotion(heures, minutes, seconds);
 
-            }
-            if (!finalitem.getType().equals(Material.AIR)) {
-                e.getInventory().setResult(finalitem);
-                PotionCraft.put(e.getInventory(), z + 1);
+                    if (CheckForEmerauldMateral(craftingitems, i + 1, l)) {
+                        finalitem.setType(item.getType());
+                        finalitem.setItemMeta(item.getItemMeta());
+                        e.getInventory().setResult(null);
+                        PotionCraft.remove(e.getInventory());
+                        z = i;
+                    }
+
+                }
+                if (!finalitem.getType().equals(Material.AIR)) {
+                    e.getInventory().setResult(finalitem);
+                    PotionCraft.put(e.getInventory(), z + 1);
+                }
             }
         }
     }
+
 }
