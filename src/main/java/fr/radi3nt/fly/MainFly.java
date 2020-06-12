7771f7a8d81dbdeb70efdd4cb5dea8d070ae3b1d
Radi3nt <redwhite.faction200@gmail.com>
2020-06-12 22:51:08 +0200

    1.2.4 Update !

--- src/main/java/fr/radi3nt/fly/MainFly.java
+++ src/main/java/fr/radi3nt/fly/MainFly.java
@@ -44,7 +44,7 @@ public final class MainFly extends JavaPlugin {
     String Prefix = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("prefix") + ChatColor.RESET);
 
     //--------------------------------------------------//
-    public static final String VERSION = "1.2.3j";
+    public static final String VERSION = "1.2.4";
     private final ConsoleCommandSender console = Bukkit.getConsoleSender();
     //--------------------------------------------------//
 
