2835fb34e40576889a60e02f52b8e507ee91b3e4
Radi3nt <redwhite.faction200@gmail.com>
2020-06-12 19:57:52 +0200

    There was a better way to do it

--- src/main/java/fr/radi3nt/fly/events/OnDrinkFlyPotion.java
+++ src/main/java/fr/radi3nt/fly/events/OnDrinkFlyPotion.java
@@ -16,6 +16,7 @@ import org.bukkit.plugin.Plugin;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,89 +29,35 @@ public class OnDrinkFlyPotion implements Listener {
         if (MainFly.getPlugin(MainFly.class).getConfig().getBoolean("fly-potions")) {
 
             if (e.getItem().getItemMeta().getDisplayName().contains(ChatColor.GOLD + "Fly Potion ")) {
+
+                String[] split = e.getItem().getItemMeta().getDisplayName().split(" ");
+                ArrayList<String> NameSplitted = new ArrayList<>();
+                NameSplitted.addAll(Arrays.asList(split));
                 int Tsec = 0;
+                for (int i = 0; i < NameSplitted.size(); i++) {
+                    String name = NameSplitted.get(i);
+                    if (name.equals("sec")) {
+                        Tsec = Integer.parseInt(NameSplitted.get(i - 1).trim());
+                    }
+                }
                 int Tmin = 0;
+                if (NameSplitted.contains("min")) {
+                    for (int i = 0; i < NameSplitted.size(); i++) {
+                        String name = NameSplitted.get(i);
+                        if (name.equals("min")) {
+                            Tmin = Integer.parseInt(NameSplitted.get(i - 1).trim());
+                        }
+                    }
+                }
                 int Th = 0;
-
-
-                int secondesO = e.getItem().getItemMeta().getDisplayName().indexOf("sec");
-                int secondesI = e.getItem().getItemMeta().getDisplayName().indexOf("min");
-
-                if (secondesI == -1) {
-                    secondesI = 13;
+                if (NameSplitted.contains("h")) {
+                    for (int i = 0; i < NameSplitted.size(); i++) {
+                        String name = NameSplitted.get(i);
+                        if (name.equals("h")) {
+                            Th = Integer.parseInt(NameSplitted.get(i - 1).trim());
+                        }
+                    }
                 }
-                if (secondesI == 15) {
-                    secondesI = 19;
-                }
-                if (secondesI == 16) {
-                    secondesI = 20;
-                }
-                char[] secondsChar = new char[20];
-                e.getItem().getItemMeta().getDisplayName().getChars(secondesI, secondesO, secondsChar, 0);
-                StringBuilder sb = new StringBuilder();
-
-                // Appends characters one by one
-                for (Character ch : secondsChar) {
-                    sb.append(ch);
-                }
-
-                // convert in string
-                String secondsString = sb.toString();
-                secondsString = secondsString.replace(" ", "");
-                Tsec = Integer.parseInt(secondsString.trim());
-
-                if (e.getItem().getItemMeta().getDisplayName().contains("min")) {
-                    int minutesO = e.getItem().getItemMeta().getDisplayName().indexOf("min");
-                    int minutesI = e.getItem().getItemMeta().getDisplayName().indexOf("h");
-
-                    if (minutesI == -1) {
-                        minutesI = 13;
-                    }
-                    if (minutesI == 11) {
-                        minutesI = 13;
-                    }
-                    if (minutesI == 12) {
-                        minutesI = 14;
-                    }
-                    char[] minutesChar = new char[20];
-                    e.getItem().getItemMeta().getDisplayName().getChars(minutesI, minutesO, minutesChar, 0);
-                    StringBuilder sb1 = new StringBuilder();
-
-                    // Appends characters one by one
-                    for (Character ch : minutesChar) {
-                        sb1.append(ch);
-                    }
-
-                    // convert in string
-                    String minutesString = sb1.toString();
-                    minutesString = minutesString.replace(" ", "");
-                    Tmin = Integer.parseInt(minutesString.trim());
-                }
-                if (e.getItem().getItemMeta().getDisplayName().contains("h")) {
-                    int heuresO = e.getItem().getItemMeta().getDisplayName().indexOf("h");
-                    int heuresI = e.getItem().getItemMeta().getDisplayName().indexOf("Plugin");
-
-                    if (heuresI == -1) {
-                        heuresI = 11;
-                    }
-                    if (heuresI == 10) {
-                        heuresI = 13;
-                    }
-                    char[] heuressChar = new char[20];
-                    e.getItem().getItemMeta().getDisplayName().getChars(heuresI, heuresO, heuressChar, 0);
-                    StringBuilder sb1 = new StringBuilder();
-
-                    // Appends characters one by one
-                    for (Character ch : heuressChar) {
-                        sb1.append(ch);
-                    }
-
-                    // convert in string
-                    String heuresString = sb1.toString();
-                    heuresString = heuresString.replace(" ", "");
-                    Th = Integer.parseInt(heuresString.trim());
-                }
-                System.out.println(Th + ":" + Tmin + ":" + Tsec);
 
 
                 Tmin = Tmin * 60;
@@ -118,23 +65,15 @@ public class OnDrinkFlyPotion implements Listener {
 
                 int all = Tsec + Tmin + Th;
 
-                System.out.println(all);
 
                 if (all != 0) {
                     Plugin plugin = MainFly.getPlugin(MainFly.class);
-
                     Map<String, Long> timer = Tempfly.timer;
                     Map<String, Integer> time = Tempfly.time;
                     ArrayList<String> flyers = Fly.flyers;
-
-
                     HashMap<Player, Integer> timem = TempCheck.timem;
-
-
                     Boolean Stackable = plugin.getConfig().getBoolean("stack-tempfly");
 
-
-                    String MessageP = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("tempfly-player"));
 
 
                     File locations = new File("plugins/FlyPlugin", "flyers.yml");
@@ -163,11 +102,6 @@ public class OnDrinkFlyPotion implements Listener {
                     FlyMethod(player, true);
                     timem.put(player, 100000);
                     timer.put(player.getName(), System.currentTimeMillis());
-                    int timeleft = time.get(player.getName());
-                    int heures = (timeleft / 3600);
-                    int minutes = ((timeleft - (timeleft / 3600) * 3600) / 60);
-                    int seconds = timeleft - (heures * 3600 + minutes * 60);
-                    String TimeleftP = MessageP.replace("%hours%", String.valueOf(heures)).replace("%minutes%", String.valueOf(minutes)).replace("%seconds%", String.valueOf(seconds));
                 }
 
             }
